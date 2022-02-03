3b94b3d2f3a52790f517e6191699c9df737baa2b
Safwan Hossain <safwanh2708@gmail.com>
2022-02-03 14:23:30 -0500

    Added basic test cases for all methods

--- Tutorial Exercises/Box3DUnitTests.java
+++ Tutorial Exercises/Box3DUnitTests.java
@@ -28,4 +28,37 @@ public class Box3DUnitTests {
         int expectedOutput = 4;
         assertEquals(output, expectedOutput);
     }
+
+    @Test
+    public void getPerimeterOfFaceTest() {
+        int[] location = {1, 2, 3};
+        int[] dimensions = {2, 2, 2};
+        box = new Box3D(location, dimensions);
+
+        int output = box.getPerimeterOfFace(Box3D.Face.FRONT);
+        int expectedOutput = 8;
+        assertEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void getVolumeTest() {
+        int[] location = {1, 2, 3};
+        int[] dimensions = {2, 2, 2};
+        box = new Box3D(location, dimensions);
+
+        int output = box.getVolume();
+        int expectedOutput = 8;
+        assertEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void getSurfaceAreaTest() {
+        int[] location = {1, 2, 3};
+        int[] dimensions = {2, 2, 2};
+        box = new Box3D(location, dimensions);
+
+        int output = box.getSurfaceArea();
+        int expectedOutput = 24;
+        assertEquals(output, expectedOutput);
+    }
 }
