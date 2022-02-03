8fd7b807b4088b975650376c635c00fcafd96d27
Safwan Hossain <safwanh2708@gmail.com>
2022-02-03 14:19:38 -0500

    Added test case for getAreaOfFace method

--- Tutorial Exercises/Box3DUnitTests.java
+++ Tutorial Exercises/Box3DUnitTests.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert .*;
+
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -9,7 +11,21 @@ public class Box3DUnitTests {
     public void getDimensionsOfFaceTest() {
         int[] location = {1, 2, 3};
         int[] dimensions = {2, 2, 2};
-        box = new Box3D(new int[]{1, 2, 3}, new int[]{2, 2, 2});
-        assertArrayEquals(box.getDimensionsOfFace(Box3D.Face.FRONT), new int[] {2, 2});
+        box = new Box3D(location, dimensions);
+
+        int[] output = box.getDimensionsOfFace(Box3D.Face.FRONT);
+        int[] expectedOutput = {2, 2};
+        assertArrayEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void getAreaOfFaceTest() {
+        int[] location = {1, 2, 3};
+        int[] dimensions = {2, 2, 2};
+        box = new Box3D(location, dimensions);
+
+        int output = box.getAreaOfFace(Box3D.Face.FRONT);
+        int expectedOutput = 4;
+        assertEquals(output, expectedOutput);
     }
 }
