b1418b00e7b8b190a4dbb77a2e481850efc75e35
Safwan Hossain <safwanh2708@gmail.com>
2022-03-02 22:13:08 -0500

    Added constructor and runnable method

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,2 +1,29 @@
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<ClientHandler>();
+    private Socket socket;
+    private BufferedWriter bufferedWriter;
+    private BufferedReader bufferedReader;
+    private String clientUsername;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);
+        } catch (IOException e) {
+            //closeEverything();
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
