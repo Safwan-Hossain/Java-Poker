4b38ecd9820175814b13b1b3c66200ce51385003
Safwan Hossain <safwanh2708@gmail.com>
2022-03-02 22:29:56 -0500

    minor changes

--- src/GameInfo.java
+++ src/GameInfo.java
@@ -5,8 +5,9 @@ public class GameInfo {
     private Game game;
     private playerActions playerAction; // The type of action performed by a player
     private String message; // Any messages from the server
-    private String playerWithTurn; // Name of player who has the current turn
+
     private String playerName; // Name of player who performed an action
+    private String playerWithTurn; // Name of player who has the current turn
     private int amount; // amount of chips a player puts in for a bet or raise (0 if fold/call/check)
 
 
@@ -14,8 +15,17 @@ public class GameInfo {
         this.game = game;
         this.message = message;
         this.playerWithTurn = playerWithTurn;
-        this.playerAction = playerAction;
         this.playerName = playerName;
+        this.playerAction = playerAction;
+        this.amount = amount;
+    }
+
+    public GameInfo(String playerName, playerActions playerAction, int amount) {
+        this.game = null;
+        this.message = null;
+        this.playerWithTurn = null;
+        this.playerName = playerName;
+        this.playerAction = playerAction;
         this.amount = amount;
     }
 
@@ -41,5 +51,10 @@ public class GameInfo {
 
     public int getAmount() {
         return amount;
+    }
+
+    @Override
+    public String toString() {
+        return playerName + " performs the action " + playerAction.name() + " for an amount of " + amount;
     }
 }
