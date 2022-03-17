3d89f9cdbb6953532fffd4c8c4111364664092ea
Safwan Hossain <safwanh2708@gmail.com>
2022-03-16 23:40:23 -0400

    Created a main controller
    - Responsible for MainMenu, MainMenuView, Client and Game
    - A controller for the entire program essentially

---
+++ src/MainController.java
@@ -0,0 +1,122 @@
+import com.sun.tools.javac.Main;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MainController {
+    private static String username;
+    private static Socket socket;
+    private static Client client;
+
+    private static String getValidUsername(Scanner scanner) {
+        String username = "";
+        while (username.strip().isBlank()) {
+            MainMenuView.askForUsername();
+            username = scanner.nextLine();
+
+            if (username.strip().isBlank()) {
+                MainMenuView.displayInvalidUsername();
+            }
+        }
+        return username;
+    }
+
+    private static int getValidOption(Scanner scanner) {
+        int option = 0;
+        int maxNumOptions = MainMenu.getMaxNumOptions();
+
+        while (option <= 0 || option > maxNumOptions) {
+            MainMenuView.displayMainMenu();
+            MainMenuView.askForMenuOption();
+            if (scanner.hasNextInt()) {
+                option = scanner.nextInt();
+                scanner.nextLine();
+            }
+
+            if (option <= 0 || option > maxNumOptions) {
+                MainMenuView.displayInvalidMenuOption();
+            }
+        }
+        return option;
+    }
+
+    private static Socket getValidSocketForServer(Scanner scanner) {
+        String serverIP = "";
+        Socket socket;
+        while (true) {
+            try {
+                MainMenuView.displayServerJoinMenu();
+                serverIP = scanner.nextLine();
+                socket = new Socket(serverIP, 100);
+                break;
+            }
+            catch (IOException e) {
+                MainMenuView.displayFailedToConnectToServer(serverIP);
+            }
+        }
+        MainMenuView.displaySuccessfulConnection();
+        return socket;
+    }
+
+    private static void startClient() {
+        if (client == null) {
+            throw new NullPointerException("CLIENT IS NULL");
+        }
+        client.startClient();
+    }
+
+    private static void hostServer(Scanner scanner) throws IOException {
+        MainMenu.hostServer();
+        InetAddress localIP = InetAddress.getLocalHost();
+        MainMenuView.displaySuccessfullyStartedServer();
+        MainMenuView.displayServerIPAddress(localIP.toString());
+
+        //Join the server being hosted
+        username = getValidUsername(scanner);
+        socket = new Socket(localIP, 100);
+        MainMenuView.displaySuccessfulConnection();
+        client = MainMenu.joinServer(socket, username);
+        startClient();
+    }
+
+    private static void joinServer(Scanner scanner) throws IOException {
+        socket = getValidSocketForServer(scanner);
+        username = getValidUsername(scanner);
+        MainMenu.joinServer(socket, username);
+        //TODO start client
+    }
+
+    private static void exitProgram() {
+        MainMenuView.displayExitingProgram();
+        MainMenu.exitProgram();
+    }
+
+    private static void performMainMenuOperation(Scanner scanner, int option) throws IOException {
+        switch (option) {
+            case 1 -> hostServer(scanner);
+            case 2 -> joinServer(scanner);
+            case 3 -> exitProgram();
+            default -> { }
+        }
+    }
+
+    public static void enterProgram() throws IOException {
+        MainMenuView.displayWelcomeScreen();
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int option = getValidOption(scanner);
+            performMainMenuOperation(scanner, option);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        enterProgram();
+    }
+
+
+
+
+}
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -4,103 +4,28 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class MainMenuController {
-    static final int MAX_NUM_OPTIONS = 3;
+public class MainMenu {
+    private static final int MAX_NUM_OPTIONS = 3;
 
-    private static String getValidUsername() {
-        Scanner scanner = new Scanner(System.in);
-        String username = "";
-
-        while (username.strip().isBlank()) {
-            MainMenuView.askForUsername();
-            username = scanner.nextLine();
-            System.out.println();
-
-            if (username.strip().isBlank()) {
-                System.out.println("Invalid username.");
-            }
-        }
-        scanner.close();
-        return username;
+    public static int getMaxNumOptions() {
+        return MAX_NUM_OPTIONS;
     }
 
-    private static int getValidOption(int maxNumOptions) {
-        Scanner scanner = new Scanner(System.in);
-        int option = 0;
-
-        while (option <= 0 || option > maxNumOptions) {
-            System.out.print("Please enter your option: ");
-            if (scanner.hasNextInt()) {
-                option = scanner.nextInt();
-            }
-            else {
-                System.out.println("Invalid option.");
-            }
-            System.out.println();
-        }
-        scanner.close();
-        return option;
-    }
-
-    private static Socket getValidSocketForServer() {
-        Scanner scanner = new Scanner(System.in);
-        String serverIP = "";
-        Socket socket;
-        while (true) {
-            try {
-                MainMenuView.displayServerJoinMenu();
-                serverIP = scanner.nextLine();
-                socket = new Socket(serverIP, 100);
-                break;
-            }
-            catch (IOException e) {
-                MainMenuView.displayFailedToConnectToServer(serverIP);
-            }
-        }
-        scanner.close();
-        return socket;
-    }
-
-    private static void joinServer()  {
-        String username = getValidUsername();
-        Socket socket = getValidSocketForServer();
-
+    public static Client joinServer(Socket socket, String username)  {
         Client client = new Client(socket, username);
-        client.startClient();
+        return client;
     }
 
-    private static void hostServer() throws IOException {
+    public static void hostServer() throws IOException {
         // Host server
         ServerSocket serverSocket = new ServerSocket(100);
         Server server = new Server(serverSocket);
-        server.startServer();
-        MainMenuView.displayServerIPAddress(InetAddress.getLocalHost().toString());
-        // Join server
-        String username = getValidUsername();
-        Socket socket = new Socket(InetAddress.getLocalHost(), 100);
-        Client client = new Client(socket, username);
-        client.startClient();
+        new Thread(() -> { server.startServer(); }).start();
+
     }
 
-    private static void exitProgram() {
+    public static void exitProgram() {
         System.exit(0);
-    }
-
-    private static void performMainMenuOperation() throws IOException {
-        MainMenuView.displayMainMenu();
-        int option = getValidOption(MAX_NUM_OPTIONS);
-
-        switch (option) {
-            case 1 -> hostServer();
-            case 2 -> joinServer();
-            case 3 -> exitProgram();
-            default -> { }
-        }
-
-    }
-
-    public static void EnterProgram() throws IOException {
-        performMainMenuOperation();
     }
 
 
