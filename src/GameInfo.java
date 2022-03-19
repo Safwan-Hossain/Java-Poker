2d4b68cd2272a300a8fde87b8ea49b22290d9569
Safwan Hossain <safwanh2708@gmail.com>
2022-03-18 23:55:38 -0400

    Removed unnecessary functions and made all variables final -Doesn't make sense for GameInfo to have variables that can change -If someone wants to send data then they would not want their data to be manipulated

--- src/GameInfo.java
+++ src/GameInfo.java
@@ -2,29 +2,12 @@ import java.io.Serializable;
 
 public class GameInfo implements Serializable {
 
-    private Game game;
-    private String playerWithTurn; // Name of player who has the current turn
-    private String message; // Any messages from the server
-
-    private String clientID; // ID of client (player) who performed action
-    private String playerName; // Name of player who performed an action
-    private PlayerAction playerAction; // The type of action performed by a player
-    private int amount; // amount of chips a player puts in for a bet or raise (0 if fold/call/check)
-
-
-    public GameInfo(Game game, String message, String playerWithTurn, PlayerAction playerAction, String playerName, int amount) {
-        this.game = game;
-        this.message = message;
-        this.playerWithTurn = playerWithTurn;
-        this.playerName = playerName;
-        this.playerAction = playerAction;
-        this.amount = amount;
-    }
+    private final String clientID; // ID of client (player) who performed action
+    private final String playerName; // Name of player who performed an action
+    private final PlayerAction playerAction; // The type of action performed by a player
+    private final int amount; // amount of chips a player puts in for a bet or raise (0 if fold/call/check)
 
     public GameInfo(String ClientID, String playerName, PlayerAction playerAction, int amount) {
-        this.game = null;
-        this.message = null;
-        this.playerWithTurn = null;
         this.clientID = ClientID;
         this.playerName = playerName;
         this.playerAction = playerAction;
@@ -32,25 +15,12 @@ public class GameInfo implements Serializable {
     }
 
 
+    // DUMMY CODE -- NOT GOING TO BE ON FINAL CODE
     public GameInfo() {
-        this.game = null;
-        this.message = "";
-        this.playerWithTurn = null;
+        this.clientID = "";
         this.playerName = "";
         this.playerAction = null;
         this.amount = 0;
-    }
-
-    public Game getGame() {
-        return game;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getPlayerWithTurn() {
-        return playerWithTurn;
     }
 
     public PlayerAction getPlayerAction() {
