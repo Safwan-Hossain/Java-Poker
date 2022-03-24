2ef83d91b6ea643b44270cc4258e5703e681c3fd
Safwan Hossain <safwanh2708@gmail.com>
2022-03-24 01:50:50 -0400

    Implmented temprorary ToString method

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,5 @@
+import java.io.Serializable;
+
 /***********************
  *  Assignment 4
  *  Andreas Hadjigeorgiou
@@ -5,7 +7,7 @@
  *  Card class
  */
 
-public class Card
+public class Card implements Serializable
 {
   // I.V.s are suit and rank
 	public int suit;
@@ -18,6 +20,26 @@ public class Card
 	     else
 	           return false;
 	}
-	
 
+	@Override
+	public String toString() {
+		String suitName = "";
+		switch (suit) {
+			case 1 -> suitName = "clubs";
+			case 2 -> suitName = "diamonds";
+			case 3 -> suitName = "hearts";
+			case 4 -> suitName = "spades";
+		}
+
+		String rankName = "";
+		switch (rank) {
+			case 1 -> rankName = "Ace";
+			case 11 -> rankName = "Jack";
+			case 12 -> rankName = "Queen";
+			case 13 -> rankName = "King";
+			default -> rankName = Integer.toString(rank);
+		}
+
+		return rankName + " of " + suitName;
+	}
 }
