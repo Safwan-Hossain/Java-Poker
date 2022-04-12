ac93181a5d68fbbf9017b010d83bda47b8f4c92d
Safwan Hossain <safwanh2708@gmail.com>
2022-04-12 19:58:24 -0400

    Finished full test for Junit

--- TestCases/src/GameTest.java
+++ TestCases/src/GameTest.java
@@ -3,7 +3,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class GameTest {
 
@@ -115,6 +117,7 @@ public class GameTest {
     // total pot is updated
     @Test
     public void performBetByPlayerTest() {
+        game.startGame();
         game.initializeRound();
         int raiseAmount = smallBlind * 5;
         Player better = players.get(1);
@@ -146,6 +149,7 @@ public class GameTest {
 
     @Test
     public void performCallByPlayerTest() {
+        game.startGame();
         game.initializeRound();
         Player smallBlindPlayer = game.getPlayersWithRoles().get(PokerRole.SMALL_BLIND);
         game.performCallByPlayer(smallBlindPlayer);
@@ -174,8 +178,7 @@ public class GameTest {
         assert (game.getTotalPot() == 2 * bigBlind);
     }
 
-    @Test
-    public void removeLosersTest() {
+    private Game getNewGameWithLosers() {
         Player loser1 = new Player("Loser", "100");
         Player loser2 = new Player("Loser", "200");
         Player loser3 = new Player("Loser", "300");
@@ -195,53 +198,42 @@ public class GameTest {
         players.add(winner1);
         players.add(winner2);
 
-        game = new Game(players, smallBlind);
+        return new Game(players, smallBlind);
+
+    }
+    @Test
+    public void removeLosersTest() {
+        game = getNewGameWithLosers();
         game.removeLosers();
 
         ArrayList<Player> expectedPlayers = new ArrayList<>();
-        expectedPlayers.add(winner1);
-        expectedPlayers.add(winner2);
+        for (Player player: game.getPlayers()) {
+            if (player.getChips() > 0) {
+                expectedPlayers.add(player);
+            }
+        }
 
         ArrayList<Player> actualPlayers = game.getPlayers();
-
         assertEquals(actualPlayers, expectedPlayers);
     }
 
     @Test
     public void getPlayersWithNoChipsTest() {
-        Player loser1 = new Player("Loser", "100");
-        Player loser2 = new Player("Loser", "200");
-        Player loser3 = new Player("Loser", "300");
-        Player winner1 = new Player("Winner", "1210");
-        Player winner2 = new Player("Winner", "1220");
-
-        loser1.set_chips(0);
-        loser2.set_chips(0);
-        loser3.set_chips(0);
-        winner1.set_chips(buyIn);
-        winner2.set_chips(buyIn);
-
-        ArrayList<Player> players = new ArrayList<>();
-        players.add(loser1);
-        players.add(loser2);
-        players.add(loser3);
-        players.add(winner1);
-        players.add(winner2);
-
-        game = new Game(players, smallBlind);
+        game = getNewGameWithLosers();
 
         ArrayList<Player> expectedPlayers = new ArrayList<>();
-        expectedPlayers.add(loser1);
-        expectedPlayers.add(loser2);
-        expectedPlayers.add(loser3);
-
+        for (Player player: game.getPlayers()) {
+            if (player.getChips() <= 0) {
+                expectedPlayers.add(player);
+            }
+        }
         ArrayList<Player> actualPlayers = game.getPlayersWithNoChips();
-
         assertEquals(actualPlayers, expectedPlayers);
     }
 
     @Test
     public void endRoundTest() {
+        game.startGame();
         game.initializeRound();
         game.endRound();
 
@@ -264,6 +256,7 @@ public class GameTest {
 
     @Test
     public void isEveryBetTheSameTest() {
+        game.startGame();
         game.initializeRound();
         assertFalse(game.isEveryBetTheSame());
 
@@ -278,6 +271,7 @@ public class GameTest {
 
     @Test
     public void applyPlayerAction() {
+        game.startGame();
         game.initializeRound();
         Player folder = game.getPlayer(players.get(0));
         Player better = game.getPlayer(players.get(1));
@@ -300,6 +294,7 @@ public class GameTest {
 
     @Test
     public void hasEveryoneHadATurnTest() {
+        game.startGame();
         game.initializeRound();
 
         assertFalse(game.hasEveryoneHadATurn());
@@ -311,6 +306,7 @@ public class GameTest {
 
     @Test
     public void isRoundStateOverTest() {
+        game.startGame();
         game.initializeRound();
 
         assertFalse(game.isRoundStateOver());
@@ -325,6 +321,7 @@ public class GameTest {
 
     @Test
     public void everyoneIsAllInTest() {
+        game.startGame();
         game.initializeRound();
         assertFalse(game.everyoneIsAllIn());
         game.performBetByPlayer(players.get(0), buyIn);
@@ -336,6 +333,7 @@ public class GameTest {
 
     @Test
     public void getPlayerWithTurnTest() {
+        game.startGame();
         game.initializeRound();
         Player bigBlindPlayer = game.getPlayersWithRoles().get(PokerRole.BIG_BLIND);
         ArrayList<Player> players = game.getPlayers();
@@ -355,21 +353,228 @@ public class GameTest {
 
     @Test
     public void giveNextPlayerTurnTest() {
+        game.startGame();
         game.initializeRound();
         Player bigBlindPlayer = game.getPlayersWithRoles().get(PokerRole.BIG_BLIND);
         ArrayList<Player> players = game.getPlayers();
 
-        int expectedPlayerWithTurnIndex = (players.indexOf(bigBlindPlayer) + 1) % players.size();
 
         for (int i = 0; i < players.size(); i++) {
-            expectedPlayerWithTurnIndex = (expectedPlayerWithTurnIndex + i) % players.size();
+            int expectedPlayerWithTurnIndex = (players.indexOf(bigBlindPlayer) + 1 + i) % players.size();
             Player expectedPlayerWithTurn = players.get(expectedPlayerWithTurnIndex);
             Player actualPlayerWithTurn = game.getPlayerWithTurn();
             assertEquals(expectedPlayerWithTurn, actualPlayerWithTurn);
             game.giveNextPlayerTurn();
         }
+    }
+
+    @Test
+    public void setPlayerRolesTest() {
+        HashMap<PokerRole, Player> expectedPlayerRoles = new HashMap<>();
+        expectedPlayerRoles.put(PokerRole.DEALER, players.get(0));
+        expectedPlayerRoles.put(PokerRole.SMALL_BLIND, players.get(1));
+        expectedPlayerRoles.put(PokerRole.BIG_BLIND, players.get(2));
+
+        game.setPlayerRoles(expectedPlayerRoles);
+        HashMap<PokerRole, Player> actualPlayerRoles = game.getPlayersWithRoles();
+
+        assertEquals(expectedPlayerRoles, actualPlayerRoles);
+        for (PokerRole role: actualPlayerRoles.keySet()) {
+            Player player =  actualPlayerRoles.get(role);
+            assertEquals(role,player.getRole());
+        }
+    }
+
+    @Test
+    public void advanceRoundStateTest() {
+        game.startGame();
+        game.initializeRound();
+
+        while (!game.getRoundState().equals(RoundState.SHOWDOWN)) {
+            RoundState expectedNextRoundState = RoundState.getNextRoundState(game.getRoundState());
+            game.advanceRoundState();
+            RoundState actualNextRoundState = game.getRoundState();
+
+            assertEquals(expectedNextRoundState, actualNextRoundState);
+        }
+    }
+
+    @Test
+    public void updateTableCardsTest() {
+        game.startGame();
+        game.initializeRound();
+
+        int expectedTableCards = 3;
+        while (!game.getRoundState().equals(RoundState.SHOWDOWN)) {
+            game.advanceRoundState();
+            assertEquals(expectedTableCards, game.getTableCards().size());
+
+            if (!game.getRoundState().equals(RoundState.RIVER)) {
+                expectedTableCards++;
+            }
+        }
+    }
+
+    private ArrayList<Card> getArbitraryTableCards() {
+        ArrayList<Card> tableCards = new ArrayList<>();
+        tableCards.add(new Card(1, 5));
+        tableCards.add(new Card(4, 9));
+        tableCards.add(new Card(1, 6));
+        tableCards.add(new Card(2, 10));
+        tableCards.add(new Card(1, 14));
+        return tableCards;
 
     }
+
+    @Test
+    public void getScoreTest() {
+        Player player = new Player("Player", "123");
+        player.addToHand(new Card(2, 2));
+        player.addToHand(new Card(3, 2));
+
+        ArrayList<Card> tableCards = getArbitraryTableCards();
+        game.setTableCards(tableCards);
+
+        int[] expectedScore = new HandEval().evaluate(player.getHand(), tableCards);
+        int[] actualScore = game.getScore(player);
+
+        assertArrayEquals(expectedScore, actualScore);
+    }
+
+
+    @Test
+    public void getPlayerHandNameTest() {
+        Player player = new Player("Player", "123");
+        player.addToHand(new Card(2, 2));
+        player.addToHand(new Card(3, 2));
+        game.setTableCards(getArbitraryTableCards());
+
+        String expectedName = HandEval.getHandName(game.getScore(player));
+        String actualName = game.getPlayerHandName(player);
+
+        assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void getHighestScoreTest() {
+        game.setTableCards(getArbitraryTableCards());
+
+        int[] expectedHighestScore = {0, 0};
+        for (Player player: game.getPlayers()) {
+            int[] playerScore = game.getScore(player);
+            if (Arrays.compare(playerScore, expectedHighestScore) > 0) {
+                expectedHighestScore = playerScore;
+            }
+        }
+        int[] actualHighestScore = game.getHighestScore();
+
+        assertArrayEquals(expectedHighestScore, actualHighestScore);
+    }
+
+    @Test
+    public void getWinningPlayersTest() {
+        game.startGame();
+        game.initializeRound();
+        game.setTableCards(getArbitraryTableCards());
+
+        ArrayList<Player> expectedWinningPlayers = new ArrayList<>();
+        ArrayList<Player> actualWinningPlayers = game.getWinningPlayers();
+        for (Player player: game.getPlayers()) {
+            if (Arrays.equals(game.getScore(player), game.getHighestScore())) {
+                expectedWinningPlayers.add(player);
+            }
+        }
+
+        assertEquals(expectedWinningPlayers, actualWinningPlayers);
+    }
+
+    @Test
+    public void giveChipsToWinnersTest() {
+        game.startGame();
+        game.initializeRound();
+        game.setTableCards(getArbitraryTableCards());
+        int totalPot = game.getTotalPot();
+        ArrayList<Player> winners = game.getWinningPlayers();
+        int winningShare = totalPot/winners.size();
+        game.giveChipsToWinners();
+
+        for (Player player: winners) {
+            if (player.getRole() == PokerRole.SMALL_BLIND) {
+                assertEquals(player.getChips(), buyIn - smallBlind + winningShare);
+            }
+            else if (player.getRole() == PokerRole.BIG_BLIND) {
+                assertEquals(player.getChips(), buyIn - bigBlind + winningShare);
+            }
+            else {
+                assertEquals(player.getChips(), buyIn + winningShare);
+            }
+        }
+    }
+
+    @Test
+    public void allOtherPlayersFoldedTest() {
+        game.startGame();
+        game.initializeRound();
+
+        for (int i = 0; i < players.size() - 1; i++) {
+            game.applyPlayerAction(players.get(i), PlayerAction.FOLD, 0);
+        }
+
+        assertTrue(game.allOtherPlayersFolded());
+    }
+
+    @Test
+    public void giveChipsToLastPlayerTest() {
+        game.startGame();
+        game.initializeRound();
+        int totalPot = game.getTotalPot();
+
+        for (int i = 0; i < players.size() - 1; i++) {
+            game.applyPlayerAction(players.get(i), PlayerAction.FOLD, 0);
+        }
+        Player lastPlayer = game.getPlayer(players.get(players.size() - 1));
+
+        game.giveChipsToLastPlayer();
+
+        if (lastPlayer.getRole() == PokerRole.SMALL_BLIND) {
+            assertEquals(buyIn - smallBlind + totalPot, lastPlayer.getChips());
+        }
+        else if (lastPlayer.getRole() == PokerRole.BIG_BLIND) {
+            assertEquals(buyIn - bigBlind + totalPot, lastPlayer.getChips());
+        }
+        else {
+            assertEquals(buyIn + totalPot, lastPlayer.getChips());
+        }
+    }
+
+    @Test
+    public void getValidActionsTest() {
+        game.startGame();
+        game.initializeRound();
+
+        HashSet<PlayerAction> expectedValidActions = new HashSet<>();
+        expectedValidActions.add(PlayerAction.CALL);
+        expectedValidActions.add(PlayerAction.RAISE);
+        expectedValidActions.add(PlayerAction.FOLD);
+
+        HashSet<PlayerAction> actualValidActions = game.getValidActions(players.get(0));
+
+        for (Player player: players) {
+            game.performCallByPlayer(player);
+        }
+
+        game.endRoundState();
+        game.advanceRoundState();
+        actualValidActions = game.getValidActions(players.get(0));
+
+        expectedValidActions = new HashSet<>();
+        expectedValidActions.add(PlayerAction.CHECK);
+        expectedValidActions.add(PlayerAction.BET);
+        expectedValidActions.add(PlayerAction.FOLD);
+
+        assertEquals(expectedValidActions, actualValidActions);
+    }
+
 //
 //    @Test(expected = RuntimeException.class)
 //    public void negativeSizeTest() {
